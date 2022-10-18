098d7aa5a224f5eaae75280d7b8aca1051f202a8
icyhot12 <rmierniczek1@gmail.com>
2022-10-18 23:27:52 +0200

    logic in progress

--- src/Question.js
+++ src/Question.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { decode } from "html-entities"
 
 function Question(props) {
+
+    const [isHeld, setIsHeld] = useState(false)
+
+    const style = {
+        backgroundColor: isHeld ? "red" : "green"
+    }
+    
+    function handleClick(event) {
+        props.handleChoose(event)
+        
+    }
+
     return (
         <div className="question-container">
             <div className="question">{props.question}</div>
             <div className="answers-container">
-                <button>{props.correct_answer}</button>
-                <button>answer2</button>
-                <button>answer3</button>
-                <button>answer4</button>
+                <button style = {style} onClick={handleClick}>{decode(props.mixed_answers[0])}</button>
+                <button style = {style} onClick={handleClick}>{decode(props.mixed_answers[1])}</button>
+                <button style = {style} onClick={handleClick}>{decode(props.mixed_answers[2])}</button>
+                <button style = {style} onClick={handleClick}>{decode(props.mixed_answers[3])}</button>
             </div>
         </div>
     )
